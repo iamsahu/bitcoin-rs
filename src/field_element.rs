00fff56d6e9ee36f7b4dee8790d9b46d69007a31
iamsahu <praffulsahu@gmail.com>
2023-02-10 12:59:20 +0530

    feat: implemented `pow`, `Div`

--- src/field_element.rs
+++ src/field_element.rs
@@ -17,8 +17,12 @@ impl FieldElement {
         Self { prime, num }
     }
 
-    pub fn pow(&self, exponent: &BigInt) -> Self {
-        let num = self.num.modpow(exponent, &self.prime);
+    pub fn pow(&self, exponent: BigInt) -> Self {
+        let mut exp = exponent;
+        if exp < 0_i32.to_bigint().unwrap() {
+            exp = exp + (self.prime.clone() - 1_i32.to_bigint().unwrap());
+        }
+        let num = self.num.modpow(&exp, &self.prime);
         Self {
             prime: self.prime.clone(),
             num,
@@ -55,9 +59,16 @@ impl ops::Sub for FieldElement {
             panic!("Cannot add elements of different primes");
         }
         let num = (self.num - other.num) % self.prime.clone();
-        Self {
-            prime: self.prime,
-            num,
+        if num < 0_i32.to_bigint().unwrap() {
+            return Self {
+                num: num + self.prime.clone(),
+                prime: self.prime,
+            };
+        } else {
+            return Self {
+                prime: self.prime,
+                num,
+            };
         }
     }
 }
@@ -70,6 +81,26 @@ impl ops::Mul for FieldElement {
             panic!("Cannot add elements of different primes");
         }
         let num = (self.num * other.num) % self.prime.clone();
+        Self {
+            prime: self.prime,
+            num,
+        }
+    }
+}
+
+impl ops::Div for FieldElement {
+    type Output = Self;
+
+    fn div(self, other: Self) -> Self {
+        if self.prime != other.prime {
+            panic!("Cannot add elements of different primes");
+        }
+        let num = (self.num
+            * other.num.modpow(
+                &(self.prime.clone() - 2_i32.to_bigint().unwrap()),
+                &self.prime,
+            ))
+            % self.prime.clone();
         Self {
             prime: self.prime,
             num,
@@ -122,14 +153,51 @@ mod tests {
 
     #[test]
     fn mul_two_field_elements() {
-        let prime = 17_i32.to_bigint().unwrap();
-        let a_num = 6_i32.to_bigint().unwrap();
-        let b_num = 13_i32.to_bigint().unwrap();
-        let c_num = 12_i32.to_bigint().unwrap();
+        let prime = 13_i32.to_bigint().unwrap();
+        let a_num = 3_i32.to_bigint().unwrap();
+        let b_num = 12_i32.to_bigint().unwrap();
+        let c_num = 10_i32.to_bigint().unwrap();
         let a = FieldElement::new(prime.clone(), a_num);
         let b = FieldElement::new(prime.clone(), b_num);
         let c = FieldElement::new(prime, c_num);
 
         assert_eq!(a * b, c);
+    }
+
+    #[test]
+    fn divide_two_field_elements() {
+        let prime = 19_i32.to_bigint().unwrap();
+        let a_num = 2_i32.to_bigint().unwrap();
+        let b_num = 7_i32.to_bigint().unwrap();
+        let c_num = 3_i32.to_bigint().unwrap();
+        let a = FieldElement::new(prime.clone(), a_num);
+        let b = FieldElement::new(prime.clone(), b_num);
+        let c = FieldElement::new(prime, c_num);
+
+        assert_eq!(a / b, c);
+    }
+
+    #[test]
+    fn power_a_field_element_to_a_positive_exponent() {
+        let prime = 13_i32.to_bigint().unwrap();
+        let a_num = 3_i32.to_bigint().unwrap();
+        let b_num = 1_i32.to_bigint().unwrap();
+        let a = FieldElement::new(prime.clone(), a_num);
+        let b = FieldElement::new(prime, b_num);
+        let exponent = 3_i32.to_bigint().unwrap();
+
+        assert_eq!(a.pow(exponent), b);
+    }
+
+    #[test]
+    fn power_a_field_element_to_a_negative_exponent() {
+        let prime = 13_i32.to_bigint().unwrap();
+        let a_num = 7_i32.to_bigint().unwrap();
+        let b_num = 8_i32.to_bigint().unwrap();
+        let a = FieldElement::new(prime.clone(), a_num);
+        let b = FieldElement::new(prime, b_num);
+        let exponent = -3_i32.to_bigint().unwrap();
+
+        assert_eq!(a.pow(exponent), b);
     }
 }
