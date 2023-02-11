ffe5ff48502a3d4be5f15c99d49e2513892c0a62
iamsahu <praffulsahu@gmail.com>
2023-02-11 12:43:29 +0530

    point refactor to use i64

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+mod field_element;
+mod point;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use num_bigint::BigInt;
+    use num_bigint::ToBigInt;
+
+    use crate::field_element;
+    use crate::point;
+
+    #[test]
+    fn test_on_curve() {
+        let prime: BigInt = 223_i32.to_bigint().unwrap();
+        let a: field_element::FieldElement =
+            field_element::FieldElement::new(prime.clone(), 0_i32.to_bigint().unwrap());
+        let b: field_element::FieldElement =
+            field_element::FieldElement::new(prime.clone(), 7_i32.to_bigint().unwrap());
+        let valid_points = [
+            point::Point::<0, 7>::new_point(
+                Some(field_element::FieldElement::new(prime.clone(), 192_i32.to_bigint().unwrap())),
+                Some(field_element::FieldElement::new(prime.clone(), 105_i32.to_bigint().unwrap())),
+            ),
+        ] 
+    }
 }
--- src/point.rs
+++ src/point.rs
@@ -1,105 +1,121 @@
-use num_bigint::BigInt;
-use num_bigint::ToBigInt;
-use std::fmt;
-use std::ops;
+use anyhow::ensure;
+use std::ops::Add;
 
-#[derive(Clone, Debug, Eq)]
-pub struct Point {
-    x: Option<BigInt>, // Allows us to represent the point at infinity. Option lets us represent the possibility of a value or the possibility of no value.
-    y: Option<BigInt>,
-    a: BigInt,
-    b: BigInt,
+#[derive(Clone, Copy, Debug)]
+pub enum Point<const A: i64, const B: i64> {
+    Infinity,
+    Point(i64, i64),
 }
 
-impl Point {
-    pub fn new(x: Option<BigInt>, y: Option<BigInt>, a: BigInt, b: BigInt) -> Self {
-        match (x.clone(), y.clone()) {
-            (Some(x_num), Some(y_num)) => {
-                if y_num.pow(2) != x_num.pow(3) + a.clone() * x_num.clone() + b.clone() {
-                    panic!("({}, {}) is not on the curve", x_num, y_num);
-                }
-            }
+impl<const A: i64, const B: i64> Point<A, B> {
+    pub fn new_point(x: i64, y: i64) -> Result<Self, anyhow::Error> {
+        ensure!(
+            y.pow(2) == x.pow(3) + A * x + B,
+            "({}, {}) is not on the curve",
+            x,
+            y
+        );
+        Ok(Point::<A, B>::Point(x, y))
+    }
 
-            (None, Some(y_num)) => {
-                panic!("Not valid!");
-            }
-            (Some(x_num), None) => {
-                panic!("Not valid!");
-            }
-            (None, None) => {}
-        }
-        Self { x, y, a, b }
+    pub fn new_infinity() -> Self {
+        Point::<A, B>::Infinity
     }
 }
 
-impl PartialEq for Point {
+impl<const A: i64, const B: i64> PartialEq for Point<A, B> {
     fn eq(&self, other: &Self) -> bool {
-        self.x == other.x && self.y == other.y && self.a == other.a && self.b == other.b
-    }
-
-    fn ne(&self, other: &Self) -> bool {
-        self.x != other.x || self.y != other.y || self.a != other.a || self.b != other.b
+        match (self, other) {
+            (Self::Infinity, Self::Infinity) => true,
+            (Self::Point(x1, y1), Self::Point(x2, y2)) => x1 == x2 && y1 == y2,
+            _ => false,
+        }
     }
 }
 
-impl ops::Add for Point {
+
+impl<const A: i64, const B: i64> Add<Point<A, B>> for Point<A, B> {
     type Output = Self;
-    fn add(self, rhs: Self) -> Self {
-        if self.a != rhs.a || self.b != rhs.b {
-            panic!(
-                "Points {}, {} are not on the same curve",
-                self.clone(),
-                rhs.clone()
-            );
+    fn add(self, other: Point<A, B>) -> Self {
+        match (self, other) {
+            (Self::Infinity, Self::Infinity) => Self::new_infinity(),
+            (Self::Infinity, Self::Point(_, _)) => other,
+            (Self::Point(_, _), Self::Infinity) => self,
+            (Self::Point(x1, y1), Self::Point(x2, y2)) if x1 == x2 && y1 != y2 => {
+                Self::new_infinity()
+            }
+            (Self::Point(x1, y1), Self::Point(x2, y2)) if x1 == x2 && (y1 == 0 || y2 == 0) => {
+                Self::new_infinity()
+            }
+            (Self::Point(x1, y1), Self::Point(x2, y2)) if x1 == x2 && y1 == y2 => {
+                let s = (3 * x1.pow(2) + A) / (2 * y1);
+                let x3 = s.pow(2) - 2 * x1;
+                let y3 = s * (x1 - x3) - y1;
+                Self::new_point(x3, y3).unwrap()
+            }
+            (Self::Point(x1, y1), Self::Point(x2, y2)) if x1 != x2 => {
+                let s = (y2 - y1) / (x2 - x1);
+                let x3 = s.pow(2) - x1 - x2;
+                let y3 = s * (x1 - x3) - y1;
+                Self::new_point(x3, y3).unwrap()
+            }
+            _ => panic!("Invalid points"),
         }
-        if self.x == None {
-            return rhs;
-        }
-        if rhs.x == None {
-            return self;
-        }
-        if self.x == rhs.x && self.y != rhs.y {
-            return Self::new(None, None, self.a, self.b);
-        }
-        if self.x != rhs.x {
-            let s = (rhs.y.clone().unwrap() - self.y.clone().unwrap())
-                / (rhs.x.clone().unwrap() - self.x.clone().unwrap());
-            let x = s.clone().pow(2) - self.x.clone().unwrap() - rhs.x.clone().unwrap();
-            let y = s * (self.x.clone().unwrap() - x.clone()) - self.y.clone().unwrap();
-            return Self::new(Some(x), Some(y), self.a, self.b);
-        }
-        if self == rhs {
-            let s = (3_i32.to_bigint().unwrap() * self.x.clone().unwrap().pow(2) + self.a.clone())
-                / (2_i32.to_bigint().unwrap() * self.y.clone().unwrap());
-            let x = s.clone().pow(2) - 2_i32.to_bigint().unwrap() * self.x.clone().unwrap();
-            let y = s * (self.x.clone().unwrap() - x.clone()) - self.y.clone().unwrap();
-            return Self::new(Some(x), Some(y), self.a.clone(), self.b);
-        }
-        if self == rhs && self.y == Some(0_i32.to_bigint().unwrap() * self.x.clone().unwrap()) {
-            return Self::new(None, None, self.a, self.b);
-        }
-        Self {
-            x: self.x,
-            y: self.y,
-            a: self.a,
-            b: self.b,
-        }
+        // if A != rhs.A || self.b != rhs.b {
+        //     panic!(
+        //         "Points {}, {} are not on the same curve",
+        //         self.clone(),
+        //         rhs.clone()
+        //     );
+        // }
+        // if self.x == None {
+        //     return rhs;
+        // }
+        // if rhs.x == None {
+        //     return self;
+        // }
+        // if self.x == rhs.x && self.y != rhs.y {
+        //     return Self::new(None, None, self.a, self.b);
+        // }
+        // if self.x != rhs.x {
+        //     let s = (rhs.y.clone().unwrap() - self.y.clone().unwrap())
+        //         / (rhs.x.clone().unwrap() - self.x.clone().unwrap());
+        //     let x = s.clone().pow(2) - self.x.clone().unwrap() - rhs.x.clone().unwrap();
+        //     let y = s * (self.x.clone().unwrap() - x.clone()) - self.y.clone().unwrap();
+        //     return Self::new(Some(x), Some(y), self.a, self.b);
+        // }
+        // if self == rhs {
+        //     let s = (3_i32.to_bigint().unwrap() * self.x.clone().unwrap().pow(2) + self.a.clone())
+        //         / (2_i32.to_bigint().unwrap() * self.y.clone().unwrap());
+        //     let x = s.clone().pow(2) - 2_i32.to_bigint().unwrap() * self.x.clone().unwrap();
+        //     let y = s * (self.x.clone().unwrap() - x.clone()) - self.y.clone().unwrap();
+        //     return Self::new(Some(x), Some(y), self.a.clone(), self.b);
+        // }
+        // if self == rhs && self.y == Some(0_i32.to_bigint().unwrap() * self.x.clone().unwrap()) {
+        //     return Self::new(None, None, self.a, self.b);
+        // }
+        // Self {
+        //     x: self.x,
+        //     y: self.y,
+        //     a: self.a,
+        //     b: self.b,
+        // }
     }
 }
 
-impl fmt::Display for Point {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match (self.x.clone(), self.y.clone()) {
-            (Some(x_num), Some(y_num)) => {
-                write!(f, "Point({},{})_{}_{}", x_num, y_num, self.a, self.b)
-            }
-            (None, None) => write!(f, "Point(infinity)_{}_{}", self.a, self.b),
-            _ => {
-                panic!("This shouldn't happen");
-            }
-        }
-    }
-}
+// impl<const A: i64, const B: i64> fmt::Display for Point<A, B> {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         match (self.x.clone(), self.y.clone()) {
+//             (Some(x_num), Some(y_num)) => {
+//                 write!(f, "Point({},{})_{}_{}", x_num, y_num, self.a, self.b)
+//             }
+//             (None, None) => write!(f, "Point(infinity)_{}_{}", self.a, self.b),
+//             _ => {
+//                 panic!("This shouldn't happen");
+//             }
+//         }
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -108,55 +124,37 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_point() {
-        let x = Some(-1_i32.to_bigint().unwrap());
-        let y = Some(-2_i32.to_bigint().unwrap());
-        let a = 5_i32.to_bigint().unwrap();
-        let b = 7_i32.to_bigint().unwrap();
-        let p = Point::new(x, y, a, b);
+        let p = Point::<5, 7>::new_point(1, 1).unwrap();
     }
 
     #[test]
-    fn test_not_equal() {
-        let x1 = Some(18_i32.to_bigint().unwrap());
-        let y1 = Some(77_i32.to_bigint().unwrap());
-        let x2 = Some(-1_i32.to_bigint().unwrap());
-        let y2 = Some(-1_i32.to_bigint().unwrap());
-        let a = 5_i32.to_bigint().unwrap();
-        let b = 7_i32.to_bigint().unwrap();
-        let p1 = Point::new(x1, y1, a.clone(), b.clone());
-        let p2 = Point::new(x2, y2, a, b);
-        assert_ne!(p1, p2);
+    fn test_ne() {
+        let p1 = Point::<5, 7>::new_point(-1, -1).unwrap();
+        let p2 = Point::<5, 7>::new_point(-1, -1).unwrap();
+        let p3 = Point::<5, 7>::new_point(-1, 1).unwrap();
+        let inf = Point::<5, 7>::new_infinity();
+
+        assert_eq!(p1, p2);
+        assert_ne!(p1, p3);
+        assert_ne!(p1, inf);
     }
 
     #[test]
     fn add_two_points_with_the_same_x() {
-        let p1_x = Some(-1_i32.to_bigint().unwrap());
-        let p1_y = Some(-1_i32.to_bigint().unwrap());
-        let a = 5_i32.to_bigint().unwrap();
-        let b = 7_i32.to_bigint().unwrap();
-        let p2_x = Some(-1_i32.to_bigint().unwrap());
-        let p2_y = Some(1_i32.to_bigint().unwrap());
-        let p1 = Point::new(p1_x, p1_y, a.clone(), b.clone());
-        let p2 = Point::new(p2_x, p2_y, a.clone(), b.clone());
-        let inf = Point::new(None, None, a.clone(), b.clone());
+        let p1 = Point::<5, 7>::new_point(-1, -1).unwrap();
+        let p2 = Point::<5,7>::new_point(-1, 1).unwrap();
+        let inf = Point::<5,7>::new_infinity();
 
-        assert_eq!(p1.clone() + inf.clone(), p1);
-        assert_eq!(inf.clone() + p2.clone(), p2);
+        assert_eq!(p1 + inf, p1);
+        assert_eq!(inf + p2, p2);
         assert_eq!(p1 + p2, inf);
     }
     #[test]
     fn add_two_points_with_different_x() {
-        let p1_x = Some(2_i32.to_bigint().unwrap());
-        let p1_y = Some(5_i32.to_bigint().unwrap());
-        let a = 5_i32.to_bigint().unwrap();
-        let b = 7_i32.to_bigint().unwrap();
-        let p2_x = Some(-1_i32.to_bigint().unwrap());
-        let p2_y = Some(-1_i32.to_bigint().unwrap());
-        let p3_x = Some(3_i32.to_bigint().unwrap());
-        let p3_y = Some(-7_i32.to_bigint().unwrap());
-        let p1 = Point::new(p1_x, p1_y, a.clone(), b.clone());
-        let p2 = Point::new(p2_x, p2_y, a.clone(), b.clone());
-        let p3 = Point::new(p3_x, p3_y, a.clone(), b.clone());
+        
+        let p1 = Point::<5, 7>::new_point(2, 5).unwrap();
+        let p2 = Point::<5, 7>::new_point(-1, -1).unwrap();
+        let p3 = Point::<5, 7>::new_point(3,-7).unwrap();
 
         assert_eq!(p1 + p2, p3);
     }
